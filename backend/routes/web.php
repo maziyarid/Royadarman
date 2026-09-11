b8588c678da0568ab65f2a5198791925e6e98acf
Maziyar Moradi <49054922+maziyarid@users.noreply.github.com>
2026-09-11 13:12:32 +0330

    Add patient new-request route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\MarketingContentController;
 use App\Http\Controllers\PanelCaseController;
 use App\Http\Controllers\PanelController;
+use App\Http\Controllers\PatientRequestController;
 use App\Http\Controllers\PublicPageController;
 use App\Http\Middleware\SetLocale;
 use Illuminate\Support\Facades\Route;
@@ -27,6 +28,7 @@ Route::prefix('{locale}')
 
         Route::middleware('auth')->group(function (): void {
             Route::get('/panel', PanelController::class)->name('panel');
+            Route::get('/panel/cases/new', [PatientRequestController::class, 'create'])->name('patient.request.create');
             Route::get('/panel/cases/{case}', [PanelCaseController::class, 'show'])->name('panel.case');
 
             Route::prefix('panel/marketing')->group(function (): void {
